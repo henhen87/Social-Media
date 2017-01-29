f03294719da7004bf84fb9fb1096e00c3ad54867
nnien001 <nnien001@gmail.com>
2017-01-29 02:14:37 -0800

    slapped together a basic display

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,11 +4,21 @@ var router = express.Router();
 var db = require('../models');
 
 router.get('/', function(req, res){
-	res.redirect('/ebert');
+	res.redirect('/movies');
 });
 
 router.get('/ebert', function(req, res){
-	res.render('movies', null);
+	//res.render('movies', null);
+});
+
+router.get('/movies', function(req, res){
+	db.events.findAll({}).then(function(dbEvents) {
+    	var hbsObject = {
+			events: dbEvents
+		};
+      	console.log(hbsObject);
+    	res.render("movies", hbsObject);
+    });
 });
 
 router.get('/profile/:id', function(req, res){
