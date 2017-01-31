b09e20060dc9449306b5ca5a35e53560cf8da060
Owner-PC\hen <btnysci@yahoo.com>
2017-01-30 21:24:10 -0800

    Changes

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var passport = require('passport');
 var router = express.Router();
 var db = require('../models');
 
@@ -15,16 +15,36 @@ router.get('/friend-book/profile', function(req, res){
 	res.render('profile');
 });
 
+router.get('/friend-book/login', function(req, res){
+	res.render('login');
+});
+
 router.get('/friend-book/register', function(res, res){
 	res.render('register');
 });
 
 router.post('/friend-book/register', function(req, res){
+
 	console.log(req.body);
+
 	db.users.create(req.body).then(function(data){
 		console.log("register data", data);
-		res.json(data);
+		// res.json(data);
+		res.render()
 	});
+
 });
+
+router.post('/friend-book/login',
+  passport.authenticate('local', { successRedirect: '/',
+                                   failureRedirect: '/friend-book/login',
+                                   failureFlash: true })
+);
+
+// app.post('/friend-book/register',
+//   passport.authenticate('local', { successRedirect: '/',
+//                                    failureRedirect: '/login',
+//                                    failureFlash: true })
+// );
 
 module.exports = router;
