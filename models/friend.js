59a71c3a90f140e1c4426863021743cfd16803ef
nnien001 <nnien001@gmail.com>
2017-01-31 22:08:25 -0800

    updated friend.js, added message.js

---
+++ models/friend.js
@@ -0,0 +1,31 @@
+module.exports = function(sequelize, DataTypes){
+  var friends = sequelize.define('friends', {
+    status: DataTypes.STRING
+  },
+
+  {
+    classMethods: {
+      associate: function(models) {
+        //we're adding two constraints here. Both friendID and userID have to be IDs in the user table, otherwise an insert will FAIL.
+        //the onDelete cascade will remove both records of the user's friends and other users that have that first user as a friend.
+        friends.belongsTo(models.users, {
+            foreignKey: 'userID',
+            targetKey: 'id', 
+            allowNull: false, 
+            onDelete: "cascade"
+        });
+
+        friends.belongsTo(models.users, {
+            foreignKey: 'friendID',
+            targetKey: 'id', 
+            allowNull: false, 
+            onDelete: "cascade"
+        });
+
+      }
+    }
+
+  });
+  return friends;
+
+}
--- models/user.js
+++ models/user.js
@@ -5,6 +5,17 @@ module.exports = function(sequelize, DataTypes){
 		password: DataTypes.STRING,
 		email: DataTypes.STRING,
 		description: DataTypes.STRING
+	},
+	{
+		classMethod: {
+			associate: function(models) {
+				users.hasMany(models.friends, {as: "userID"});
+				users.hasMany(models.friends, {as: "friendID"});
+				users.hasMany(models.messages, {as: "userID"});
+				users.hasMany(models.messages, {as: "friendID"});
+			}
+		}
+
 	});
 	return users;
 }
